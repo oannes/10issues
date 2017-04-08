f5de30dfe2c20d26f5395e1debcfd9c1d2ed8340
oannes <johannes.koponen@gmail.com>
2017-04-08 19:38:02 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(window).endlessScroll({
+		inflowPixels: 300,
+		callback: function() {
+			var $site = $('#sites li:nth-last-child(5)').clone();
+			$('#sites').append($site);
+		}
+	});
+});

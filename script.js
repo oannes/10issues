aa93b179bed10ec22b9bf02f44ef9151059db04f
oannes <johannes.koponen@gmail.com>
2017-04-08 19:45:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-	$(window).endlessScroll({
-		inflowPixels: 300,
-		callback: function() {
-			var $site = $('#sites li:nth-last-child(2)').clone();
-			$('#sites').append($site);
+	var win = $(window);
+
+	// Each time the user scrolls
+	win.scroll(function() {
+		// End of the document reached?
+		if ($(document).height() - win.height() == win.scrollTop()) {
+			$('#loading').show();
+
+			$.ajax({
+				url: 'get-post.php',
+				dataType: 'html',
+				success: function(html) {
+					$('#posts').append(html);
+					$('#loading').hide();
+				}
+			});
 		}
 	});
 });

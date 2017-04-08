0cc4107af7e0718f5497b86025986989029b41b1
oannes <johannes.koponen@gmail.com>
2017-04-08 19:42:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(window).endlessScroll({
 		inflowPixels: 300,
 		callback: function() {
-			var $site = $('#sites li:nth-last-child(3)').clone();
+			var $site = $('#sites li:nth-last-child(2)').clone();
 			$('#sites').append($site);
 		}
 	});
